2e3b8b8655ea0821e2d4fdac41157d466f29238a
Шмелев Иван <ishmelev@1ditis.ru>
2019-07-21 01:01:11 +0300

    update fix

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,7 +1,9 @@
 export const openMenu = () => {
   const menu = document.querySelector('.menu');
+  const content = document.querySelector('.content');
   menu.addEventListener('click', e => {
     menu.classList.toggle('open');
+    content.classList.toggle('open');
   });
   return true;
 }
--- index.php
+++ index.php
@@ -4,8 +4,8 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="/assets/css/style.css">
-  <script type="module" src="/assets/js/main.js"></script>
+  <link rel="stylesheet" href="./assets/css/style.css">
+  <script type="module" src="./assets/js/main.js"></script>
   <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
   <title>Document</title>
 </head>
