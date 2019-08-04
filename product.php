0581b26d3e02c92e19afdf12001ef0aece86dcd3
Шмелев Иван <ishmelev@1ditis.ru>
2019-08-04 23:55:26 +0300

    product page

---
+++ product.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="./assets/css/style.css">
+  <script type="module" src="./assets/js/main.js"></script>
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
+  <title>Товар</title>
+</head>
+<body>
+  <div class="wrapper">
+    <section class="header">
+      <?php require_once ('header.html'); ?>
+    </section>
+    <section class="content">
+      <div class="breadcrumbs">
+        <a href="./index.php" class="link">Главная</a>
+        <span class="link">&nbsp;/&nbsp;Шубы&nbsp;</span>
+        <span class="link">&nbsp;/&nbsp;Пальто&nbsp;</span>
+      </div>
+      <?php require_once ('productPage.html'); ?>
+       <div class="title_discount_page left">
+        <h2>Вы смотрели</h2>
+      </div>
+      <?php require ('new_collection.html'); ?>
+      <div class="title_discount_page left">
+        <h2>Рекомендуемые товары</h2>
+      </div>
+      <?php require ('new_collection.html'); ?>
+    </section>
+    <section class="footer">
+      <?php require_once ('footer.html'); ?>
+    </section>
+  </div>
+</body>
+</html>
